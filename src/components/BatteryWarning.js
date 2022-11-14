6b6cd7c5dba0be6686da7a7c3485dcad62c6f1bf
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2022-11-14 14:37:34 +0100

    backup 14-11
    added contact, TO DO: CSS

--- src/components/BatteryWarning.js
+++ src/components/BatteryWarning.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./BatteryWarning.css";
 
 function BatteryWarning({ extremeTempList }) {
-  return (
-    <div className="tooltip">
-      <img
-        src={"https://www.svgrepo.com/show/361848/warning.svg"}
-        alt="battery warning"
-        className={`battery${extremeTempList ? "-warning" : ""}`}
-      />
-    </div>
-  );
+	return (
+		<div className="tooltip">
+			<img
+				src={"https://www.svgrepo.com/show/361848/warning.svg"}
+				alt="battery warning"
+				className={`battery${extremeTempList ? "-warning" : ""}`}
+			/>
+		</div>
+	);
 }
 
 export default BatteryWarning;
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,97 +4,102 @@ import { GEO_API_URL, geoApiOptions } from "../API";
 import "./Search.css";
 
 const Search = ({ onSearchChange, place }) => {
-  const [search, setSearch] = useState(null);
+	const [search, setSearch] = useState(null);
 
-  const placeSuggestions = inputValue => {
-    if (inputValue) {
-      return fetch(
-        `${GEO_API_URL}/cities?namePrefix=${inputValue}&radius=20&distanceUnit=KM`,
-        geoApiOptions
-      )
-        .then(response => response.json())
-        .then(response => {
-          return {
-            options: response.data.map(city => {
-              return {
-                value: `${city.latitude} ${city.longitude}`,
-                label: `${city.name}, ${city.countryCode}`,
-              };
-            }),
-          };
-        })
-        .catch(err => console.error(err));
-    }
-  };
+	const placeSuggestions = inputValue => {
+		if (inputValue) {
+			return fetch(
+				`${GEO_API_URL}/cities?namePrefix=${inputValue}&radius=20&distanceUnit=KM`,
+				geoApiOptions
+			)
+				.then(response => response.json())
+				.then(response => {
+					return {
+						options: response.data.map(city => {
+							return {
+								value: `${city.latitude} ${city.longitude}`,
+								label: `${city.name}, ${city.countryCode}`,
+							};
+						}),
+					};
+				})
+				.catch(err => console.error(err));
+		}
+	};
 
-  const handleOnChange = searchData => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
+	const handleOnChange = searchData => {
+		setSearch(searchData);
+		onSearchChange(searchData);
+	};
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(geoLocation => {
-      console.log("Geolocation available.");
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(geoLocation => {
+			console.log("Geolocation available.");
 
-      onSearchChange({
-        value: `${geoLocation.coords.latitude} ${geoLocation.coords.longitude}`,
-      });
-    });
-  }, []);
+			onSearchChange({
+				value: `${geoLocation.coords.latitude} ${geoLocation.coords.longitude}`,
+			});
+		});
+	}, []);
 
-  const customStyles = {
-    input: provided => ({
-      ...provided,
-      cursor: "text",
-      color: "rgba(255,255,255,0.8)",
-    }),
-    control: state => ({
-      border: "1px solid rgb(255,255,255, 0.8)",
-      borderRadius: "5px",
-      color: "rgba(255,255,255,0.8)",
-    }),
-    indicatorsContainer: () => ({
-      display: "none",
-    }),
-    indicatorSeparator: () => ({
-      display: "none",
-    }),
-    dropdownIndicator: () => ({
-      display: "none",
-    }),
-    menu: () => ({
-      position: "absolute",
-      width: "100%",
-      color: "rgba(255,255,255,0.8)",
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      // color: state.isSelected ? 'red' : 'blue',
-      background: "rgb(0,0,0)",
-      backdropFilter: "blur(5px)",
-      opacity: state.isFocused ? "1" : "0.8",
-      cursor: "pointer",
-    }),
-    singleValue: state => ({
-      position: "absolute",
-      paddingLeft: "10px",
-    }),
-  };
+	const customStyles = {
+		input: provided => ({
+			...provided,
+			cursor: "text",
+			color: "rgba(255,255,255)",
+		}),
+		control: state => ({
+			border: "1px solid rgb(255,255,255)",
+			borderRadius: "5px",
+			color: "rgba(255,255,255)",
+		}),
+		indicatorsContainer: () => ({
+			display: "none",
+		}),
+		indicatorSeparator: () => ({
+			display: "none",
+		}),
+		dropdownIndicator: () => ({
+			display: "none",
+		}),
+		menu: () => ({
+			position: "absolute",
+			width: "100%",
+			color: "rgba(255,255,255)",
+		}),
+		option: (provided, state) => ({
+			...provided,
+			// color: state.isSelected ? 'red' : 'blue',
+			background: "rgb(0,0,0)",
+			backdropFilter: "blur(5px)",
+			opacity: "1",
+			cursor: "pointer",
+		}),
+		singleValue: state => ({
+			position: "absolute",
+			paddingLeft: "10px",
+		}),
+		placeholder: () => ({
+			color: "white",
+			position: "absolute",
+			marginLeft: "10px"
+		})
+	};
 
-  return (
-    <AsyncPaginate
-      placeholder={place}
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={placeSuggestions}
-      className="searchbar"
-      classNamePrefix="searchbar"
-      styles={customStyles}
-      //loadingMessage
-      //noOptionsMessage
-    />
-  );
+	return (
+		<AsyncPaginate
+			placeholder={place}
+			debounceTimeout={600}
+			value={search}
+			onChange={handleOnChange}
+			loadOptions={placeSuggestions}
+			className="searchbar"
+			classNamePrefix="searchbar"
+			styles={customStyles}
+		//loadingMessage
+		//noOptionsMessage
+		/>
+	);
 };
 
 export default Search;
